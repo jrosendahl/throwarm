f52cb0fcb8a902309e7a25675f4330512ad40e96
John Rosendahl <jrosendahl@spindrift.(none)>
2014-03-19 20:57:15 -0600

    Fixed spellign of Children

--- test/test.js
+++ test/test.js
@@ -13,7 +13,7 @@ var row = [];
 row[0] = {};
 row[0].mainID = 123;
 row[0].mainName = 'Parent';
-row[0].mainProperty = 'Taller than childern';
+row[0].mainProperty = 'Taller than children';
 row[0].childID = 1;
 row[0].childName = 'Billie';
 row[0].childProperty = 'Fast';
@@ -22,7 +22,7 @@ row[0].childType = 'Good';
 row[1] = {};
 row[1].mainID = 123;
 row[1].mainName = 'Parent';
-row[1].mainProperty = 'Taller than childern';
+row[1].mainProperty = 'Taller than children';
 row[1].childID = 2;
 row[1].childName = 'Suzie';
 row[1].childProperty = 'Devious';
@@ -31,7 +31,7 @@ row[1].childType = 'Bad';
 row[2] = {};
 row[2].mainID = 123;
 row[2].mainName = 'Parent';
-row[2].mainProperty = 'Taller than childern';
+row[2].mainProperty = 'Taller than children';
 row[2].childID =3;
 row[2].childName ='Jamie';
 row[2].childProperty = 'Stable';
@@ -40,7 +40,7 @@ row[2].childType = 'Good';
 row[3] = {};
 row[3].mainID = 123;
 row[3].mainName = 'Parent';
-row[3].mainProperty = 'Taller than childern';
+row[3].mainProperty = 'Taller than children';
 row[3].childID =4;
 row[3].childName ='Cindy';
 row[3].childProperty = 'Fast';
@@ -92,9 +92,9 @@ Child.prototype.isFast = function() {
 
 
 
-function Childern() {
-	var childern = new Dataset();
-	childern.typeName= 'Childern';
+function Children() {
+	var children = new Dataset();
+	children.typeName= 'Children';
 
 	//I am adding a function to the object
 	function getByType(type) {
@@ -102,9 +102,9 @@ function Childern() {
 		function byType(child) {
 			return child.type === type;
 		}
-		return self.copyReturn(self.filter(byType), new Childern());
+		return self.copyReturn(self.filter(byType), new Children());
 	}
-	Object.defineProperty(childern,'getByType',{value:getByType,writable:false,configurable:false,enumerable:false});
+	Object.defineProperty(children,'getByType',{value:getByType,writable:false,configurable:false,enumerable:false});
 	
 	function getById(id) {
 		var self = this;
@@ -118,17 +118,16 @@ function Childern() {
 		return child[0];
 	}
 
-	Object.defineProperty(childern,'getById',{value:getById,writable:false,configurable:false,enumerable:false});
+	Object.defineProperty(children,'getById',{value:getById,writable:false,configurable:false,enumerable:false});
 	
 	//Once the definition is complete you must return the object
-	return childern;
+	return children;
 }
 
 //This is one way to look at the data
 var parentWithKids = new Parent();
-parentWithKids.fieldMap.kids = new Childern();
+parentWithKids.fieldMap.kids = new Children();
 parentWithKids.fieldMap.kids.key = 'childID';
-
 parentWithKids.fieldMap.kids.items = function () {
 	var child = new Child();
 	return child;
@@ -136,7 +135,7 @@ parentWithKids.fieldMap.kids.items = function () {
 
 
 //This is a different way to look at the same data
-var kidsWithParent = new Childern();
+var kidsWithParent = new Children();
 kidsWithParent.key = 'childID';
 kidsWithParent.items= function() {
 	var child = new Child();
@@ -155,29 +154,29 @@ describe('The Throwarm Library', function() {
 			parentWithKids.kids.length.should.equal(4);
 			parentWithKids.kids[0].id.should.equal(1);
 		});
-		it('calling getByType should return a subset of Childern maintaing type', function() {
+		it('calling getByType should return a subset of Children maintaing type', function() {
 			var goodKids = parentWithKids.kids.getByType('Good');
 			goodKids.length.should.equal(3);
 			//Can;t really use instance of (since it is an array so check byu callign function)
-			goodKids.typeName.should.equal('Childern');
+			goodKids.typeName.should.equal('Children');
 			goodKids.getById(1).should.be.instanceOf(Child);
 			goodKids.getById(1).name.should.equal('Billie');
 			//since the bad kid was filered out it does not exist;
 			(goodKids.getById(2) === null).should.be.true;
 		});
-		it('The childern should have isFast set', function() {
+		it('The children should have isFast set', function() {
 			parentWithKids.kids[0].isFast().should.be.true;
 			parentWithKids.kids[2].isFast().should.be.false;
 
 		});
-		it('has for the childern should work', function(){
+		it('has for the children should work', function(){
 			parentWithKids.kids.has('Child').should.be.true;
 			parentWithKids.kids.has('Parent').should.be.false;
 			parentWithKids.kids.has('I Do Not Exist').should.be.false;
 		});
 		it('has for the parent should work', function(){
-			parentWithKids.has('kids', 'Childern').should.be.true;
-			parentWithKids.has('NA', 'Childern').should.be.false;
+			parentWithKids.has('kids', 'Children').should.be.true;
+			parentWithKids.has('NA', 'Children').should.be.false;
 			parentWithKids.has('kids', 'NA').should.be.false;
 			parentWithKids.has('NA', 'NA').should.be.false;
 		});
